98080e78f708d8a2a975e68b53476fe30d446e53
Dean Galvin <dean.galvin3@gmail.com>
2019-09-29 20:41:12 -0400

    added service module

---
+++ service/service.tf
@@ -0,0 +1,169 @@
+locals {
+  tcp_ports = [for port in var.forward_tcp: {
+    name           = "TCP_${port}"
+    target_port    = port
+    published_port = port
+    protocol       = "tcp"
+  }]
+  udp_ports = [for port in var.forward_udp: {
+    name           = "UDP_${port}"
+    target_port    = port
+    published_port = port
+    protocol       = "udp"
+  }]
+  ports = concat(local.tcp_ports, local.udp_ports)
+
+
+  read_only_paths_normalized = [for paths in var.read_only_paths: {
+    target = paths.c
+    source = paths.h
+    type = "bind"
+    read_only = true
+  }]
+
+  fast_paths_normalized = [for path in var.fast_paths: {
+    target = path
+    source = "/storage/fast/${var.name}${path}"
+    type = "bind"
+    read_only = false
+  }]
+  paths = flatten([
+    {
+      target = "/etc/localtime",
+      source = "/etc/localtime",
+      type = "bind",
+      read_only = true,
+    },
+    var.config_path == null ? [] : [{
+      target = var.config_path
+      source = "/storage/cold/opt/${var.name}"
+      read_only = false
+      type = "bind"
+    }],
+    var.root_path == null ? [] : [{
+      target = var.root_path
+      source = "/"
+      read_only = true
+      type = "bind"
+    }],
+    var.dockersock_path == null ? [] : [{
+      target = var.dockersock_path
+      source = "/var/run/docker.sock"
+      read_only = true
+      type = "bind"
+    }],
+    var.storage_path == null ? [] : [{
+      target = var.storage_path
+      source = "/storage/cold"
+      read_only = false
+      type = "bind"
+    }],
+    var.seedbox_path == null ? [] : [{
+      target = var.seedbox_path
+      source = "/storage/cold/seedbox"
+      read_only = true
+      type = "bind"
+    }],
+    var.tv_path == null ? [] : [{
+      target = var.tv_path
+      source = "/storage/cold/tv"
+      read_only = false
+      type = "bind"
+    }],
+    var.movies_path == null ? [] : [{
+      target = var.movies_path
+      source = "/storage/cold/movies"
+      read_only = false
+      type = "bind"
+    }],
+    local.read_only_paths_normalized,
+    local.fast_paths_normalized,
+  ])
+
+  labels = merge(
+    {},
+    var.web_access_port == null ? {} : {
+      "traefik.frontend.rule" = "Host:${var.name}.deangalvin.com"
+      "traefik.frontend.entryPoints" = "https"
+      "traefik.tags" = "traefik-public"
+      "traefik.docker.network" = "traefik-public"
+      "traefik.port" = var.web_access_port
+    }
+  )
+
+  networks = flatten([
+    var.web_access_port == null ? [] : ["ifjk2fi7q9qusm7p5p5q594l6"],
+  ])
+}
+resource "docker_service" "service" {
+  name = var.name
+
+  task_spec {
+    networks = local.networks
+
+    container_spec {
+      image = "${var.image}:${var.image_version}"
+      args  = var.args
+      labels = local.labels
+      env = var.env
+
+      dynamic "dns_config" {
+        for_each = range(var.dns_nameservers == null ? 0 : 1)
+        content {
+          nameservers = var.dns_nameservers
+        }
+      }
+
+      dynamic "mounts" {
+        for_each = local.paths
+        content {
+          target = mounts.value.target
+          source = mounts.value.source
+          type = mounts.value.type
+          read_only = mounts.value.read_only
+        }
+      }
+    }
+  }
+
+  endpoint_spec {
+    dynamic "ports" {
+      for_each = local.ports
+      content {
+        target_port    = ports.value.target_port
+        published_port = ports.value.published_port
+        protocol       = ports.value.protocol
+      }
+    }
+  }
+
+  mode {
+    replicated {
+      replicas = var.scale
+    }
+  }
+}
+
+data "docker_registry_image" "container_registry_image" {
+  name = "${var.image}:${var.image_version}"
+}
+
+resource "docker_image" "container_image" {
+  name          = "${data.docker_registry_image.container_registry_image.name}"
+  pull_triggers = ["${data.docker_registry_image.container_registry_image.sha256_digest}"]
+  keep_locally  = true
+}
+
+data "http" "ip_address" {
+  url = "https://wtfismyip.com/text"
+}
+
+resource "cloudflare_record" "a_record" {
+  count = "${var.web_access_port == null ? 0 : 1}"
+  domain = "deangalvin.com"
+  name = "${var.name}"
+  value = "${chomp(data.http.ip_address.body)}"
+  ttl = 1
+  type = "A"
+  proxied = "true"
+}
---
+++ service/variables.tf
@@ -0,0 +1,117 @@
+variable "name" {
+  description = "The name of the service"
+  type        = string
+}
+
+variable "image" {
+  description = "The docker image name to use within this service"
+  type        = string
+}
+
+variable "image_version" {
+  description = "The image verison to use"
+  type        = string
+  default     = "latest"
+}
+
+variable "args" {
+  description = "Arguments to pass to the containers entrypoint command"
+  type        = list(string)
+  default     = null
+}
+
+variable "env" {
+  description = "Environment variables to be passed into the tasks"
+  default = {}
+}
+
+variable "web_access_port" {
+  description = "Port to forward for web access"
+  default = null
+  type = number
+}
+
+variable "forward_tcp" {
+  description = "TCP Ports to forward"
+  default     = []
+  type        = list(number)
+}
+
+variable "forward_udp" {
+  description = "UDP Ports to forward"
+  default     = []
+  type        = list(number)
+}
+
+variable "dns_nameservers" {
+  description = "Name servers to use"
+  default = null
+  type = list(string)
+}
+
+variable "config_path" {
+  description = "The path to the configuration on the container"
+  default     = null
+  type        = string
+}
+
+variable "dockersock_path" {
+  description = "The path to the dockersock on the container"
+  default     = null
+  type        = string
+}
+
+variable "tv_path" {
+  description = "The path to the tv on the container"
+  default     = null
+  type        = string
+}
+
+variable "movies_path" {
+  description = "The path to the movies on the container"
+  default     = null
+  type        = string
+}
+
+variable "seedbox_path" {
+  description = "The path to the seedbox on the container"
+  default     = null
+  type        = string
+}
+
+variable "storage_path" {
+  description = "The path to the cold storage on the container"
+  default     = null
+  type        = string
+}
+
+variable "root_path" {
+  description = "The path to the root dir on the container"
+  default     = null
+  type        = string
+}
+
+variable "fast_paths" {
+  description = "Paths to be mounted for fast access (like transcoding)"
+  default     = []
+  type        = list(string)
+}
+
+variable "read_only_paths" {
+  description = "Additional read only mounts"
+  default     = []
+  type        = list(object({
+    c = string
+    h = string
+  }))
+}
+
+variable "scale" {
+  description = "Override for scaling how many containers should exist for this service"
+  default = 2
+}
+
+variable "vpn" {
+  description = "Name of the VPN to connect to"
+  default = ""
+}
