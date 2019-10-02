95cdbb7e31f16e829e2beb43fe7d545cae588c47
Dean Galvin <dean.galvin3@gmail.com>
2019-10-01 22:33:19 -0400

    Added proper ip I think

--- vm/main.tf
+++ vm/main.tf
@@ -2,8 +2,22 @@ locals {
   data = {
     "docker" = {
       os = "/home/bananaboy/Projects/tftest/coreos_production_qemu_image.img"
+      network_name = "dockernet"
+      ip_prefix = "10.1.1"
+    },
+    "k8s" = {
+      os = "https://github.com/rancher/k3os/releases/download/v0.3.0/k3os-amd64.iso"
+      network_name = "k8snet"
+      ip_prefix = "10.1.2"
     }
   }
+  ip_prefix = local.data[var.orchistration_type]["ip_prefix"]
+  controller_ip = local.ip_prefix+".11"
+  ip_cidr = local.ip_prefix+".0/24"
+  os = local.data[var.orchistration_type]["os"]
+  network_name = local.data[var.orchistration_type]["network_name"]
+  network_domain = local.network_name + ".local"
+  memory = 8192 / var.node_count
 }
 
 resource "libvirt_pool" "pool" {
@@ -18,11 +32,29 @@ resource "libvirt_ignition" "ignition" {
   content = data.ignition_config.startup.rendered
 }
 
+resource "libvirt_network" "main_net" {
+  name = local.network_name
+  addresses = [local.ip_cidr]
+  domain = local.network_domain
+  dns {
+    enabled = true
+
+    hosts {
+      hostname = "controller." + local.network_domain
+      ip = local.controller_ip
+    }
+  }
+
+  dhcp {
+    enabled = true
+  }
+}
+
 # We fetch the latest ubuntu release image from their mirrors
 resource "libvirt_volume" "os" {
   name   = "os-qcow2_${var.orchistration_type}.${count.index}"
   pool   = libvirt_pool.pool.name
-  source = local.data[var.orchistration_type]["os"]
+  source = local.os
   format = "qcow2"
   count = var.node_count
 }
@@ -30,13 +62,13 @@ resource "libvirt_volume" "os" {
 resource "libvirt_domain" "vm" {
   name = "${var.orchistration_type}.${count.index}"
   coreos_ignition = libvirt_ignition.ignition.id
-  memory = 2048
+  memory = local.memory
 
   filesystem {
     source   = "/storage"
     target   = "storage"
     readonly = false
-    accessmode = "passthrough"
+    accessmode = "mapped"
   }
 
   disk {
@@ -48,7 +80,7 @@ resource "libvirt_domain" "vm" {
   }
 
   network_interface {
-    network_name = "dockernet"
+    network_id = libvirt_network.main_net.id
     addresses = ["10.0.1.1${count.index + 1}"]
     mac = "52:54:00:6c:3c:0${count.index + 1}"
     wait_for_lease = true
