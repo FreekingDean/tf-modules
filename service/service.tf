0b819411ed345de158cd12a3955b6a5f81196797
Dean <deangalvin3@gmail.com>
2020-02-21 15:24:52 -0500

    Fix tls

--- service/service.tf
+++ service/service.tf
@@ -2,9 +2,7 @@ locals {
   annotations = var.public ? {
     "traefik.ingress.kubernetes.io/frontend-entry-points" = "https-public"
   } : {
-    "traefik.ingress.kubernetes.io/router.tls.certresolver" = "default"
-    "traefik.ingress.kubernetes.io/router.tls.domains.0.main" = "deangalvin.com"
-    "traefik.ingress.kubernetes.io/router.tls.domains.0.sans" = "*.local.deangalvin.com"
+    "traefik.ingress.kubernetes.io/router.tls" = "true"
   }
 
   publicity = var.public ? "public" : "local"
