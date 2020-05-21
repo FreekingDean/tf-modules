01e3b745376deac437a5b0254cfdf1e6297dd9ba
Dean Galvin <deangalvin3@gmail.com>
2020-05-20 22:54:36 -0400

    Set PV

--- service/deployment.tf
+++ service/deployment.tf
@@ -1,37 +1,37 @@
 locals {
   privileged = length(var.added_devices) > 0 || var.privileged
-  read_only_paths_normalized = [for paths in var.read_only_paths: {
-    target = paths.c
-    source = paths.h
-    type = "bind"
+  read_only_paths_normalized = [for paths in var.read_only_paths : {
+    target    = paths.c
+    source    = paths.h
+    type      = "bind"
     read_only = true
   }]
 
-  read_write_files_normalized = [for paths in var.read_write_files: {
-    target = paths.c
-    source = paths.h
-    type = "file"
+  read_write_files_normalized = [for paths in var.read_write_files : {
+    target    = paths.c
+    source    = paths.h
+    type      = "file"
     read_only = false
   }]
 
-  read_write_paths_normalized = [for paths in var.read_write_paths: {
-    target = paths.c
-    source = paths.h
-    type = "bind"
+  read_write_paths_normalized = [for paths in var.read_write_paths : {
+    target    = paths.c
+    source    = paths.h
+    type      = "bind"
     read_only = false
   }]
 
-  fast_paths_normalized = [for path in var.fast_paths: {
-    target = path
-    source = "/storage/fast/${var.name}${path}"
-    type = "bind"
+  fast_paths_normalized = [for path in var.fast_paths : {
+    target    = path
+    source    = "/storage/fast/${var.name}${path}"
+    type      = "bind"
     read_only = false
   }]
 
-  devices_normalized = [for path in var.added_devices: {
-    target = path
-    source = path
-    type = "bind"
+  devices_normalized = [for path in var.added_devices : {
+    target    = path
+    source    = path
+    type      = "bind"
     read_only = false
   }]
 
@@ -48,40 +48,40 @@ locals {
     #  read_only = true,
     #},
     var.config_path == null ? [] : [{
-      target = var.config_path
-      source = "/storage/cold/opt/${var.name}"
+      target    = var.config_path
+      source    = "/storage/cold/opt/${var.name}"
       read_only = false
-      type = "bind"
+      type      = "bind"
     }],
     var.root_path == null ? [] : [{
-      target = var.root_path
-      source = "/"
+      target    = var.root_path
+      source    = "/"
       read_only = true
-      type = "bind"
+      type      = "bind"
     }],
     var.storage_path == null ? [] : [{
-      target = var.storage_path
-      source = "/storage/cold"
+      target    = var.storage_path
+      source    = "/storage/cold"
       read_only = false
-      type = "bind"
+      type      = "bind"
     }],
     var.seedbox_path == null ? [] : [{
-      target = var.seedbox_path
-      source = "/storage/cold/seedbox"
+      target    = var.seedbox_path
+      source    = "/storage/cold/seedbox"
       read_only = false
-      type = "bind"
+      type      = "bind"
     }],
     var.tv_path == null ? [] : [{
-      target = var.tv_path
-      source = "/storage/cold/tv"
+      target    = var.tv_path
+      source    = "/storage/cold/tv"
       read_only = false
-      type = "bind"
+      type      = "bind"
     }],
     var.movies_path == null ? [] : [{
-      target = var.movies_path
-      source = "/storage/cold/movies"
+      target    = var.movies_path
+      source    = "/storage/cold/movies"
       read_only = false
-      type = "bind"
+      type      = "bind"
     }],
     local.read_only_paths_normalized,
     local.read_write_paths_normalized,
@@ -146,9 +146,8 @@ resource "kubernetes_deployment" "deployment" {
           for_each = local.paths
           content {
             name = "vol-${volume.key}"
-            host_path {
-              type = volume.value.type == "file" ? "FileOrCreate" : null
-              path = volume.value.source
+            persistent_volume_path {
+              claim_name = split("/", volume.value.source)[2]
             }
           }
         }
@@ -168,7 +167,7 @@ resource "kubernetes_deployment" "deployment" {
             dynamic "env" {
               for_each = var.env
               content {
-                name = env.key
+                name  = env.key
                 value = env.value
               }
             }
@@ -177,9 +176,9 @@ resource "kubernetes_deployment" "deployment" {
               for_each = local.paths
 
               content {
-                name = "vol-${volume_mount.key}"
+                name       = "vol-${volume_mount.key}"
                 mount_path = volume_mount.value.target
-                read_only = volume_mount.value.read_only
+                sub_path   = slice(split("/", volume_mount.value.source), 3, length(volume_mount.value.source))
               }
             }
 
@@ -197,9 +196,9 @@ resource "kubernetes_deployment" "deployment" {
           image = "${var.image}:${var.image_version}"
 
           security_context {
-            privileged = length(var.added_devices) > 0 ? true : false
+            privileged      = length(var.added_devices) > 0 ? true : false
             run_as_non_root = var.run_as_user == 0 ? null : true
-            run_as_user = var.run_as_user == 0 ? null : var.run_as_user
+            run_as_user     = var.run_as_user == 0 ? null : var.run_as_user
             capabilities {
               add = local.capabilities
             }
@@ -208,7 +207,7 @@ resource "kubernetes_deployment" "deployment" {
           dynamic "env" {
             for_each = var.env
             content {
-              name = env.key
+              name  = env.key
               value = env.value
             }
           }
@@ -263,10 +262,10 @@ resource "kubernetes_deployment" "deployment" {
             for_each = var.config_file == null ? [] : [true]
 
             content {
-              name = "config-file"
+              name       = "config-file"
               mount_path = "${var.config_file.directory}/${var.config_file.filename}"
-              sub_path = var.config_file.filename
-              read_only = true
+              sub_path   = var.config_file.filename
+              read_only  = true
             }
           }
 
@@ -274,14 +273,14 @@ resource "kubernetes_deployment" "deployment" {
             for_each = local.paths
 
             content {
-              name = "vol-${volume_mount.key}"
+              name       = "vol-${volume_mount.key}"
               mount_path = volume_mount.value.target
-              read_only = volume_mount.value.read_only
+              read_only  = volume_mount.value.read_only
             }
           }
 
-          args = var.additional_args
-          tty = var.tty
+          args  = var.additional_args
+          tty   = var.tty
           stdin = var.tty
         }
       }
